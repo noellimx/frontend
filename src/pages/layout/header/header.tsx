cd7bc6d9bb359e6821cccee9606c5cb1375df1eb
Noel Lim <noellimx@gmail.com>
2023-08-21 01:56:51 +0800

    add route

--- src/pages/layout/header/header.tsx
+++ src/pages/layout/header/header.tsx
@@ -1,9 +1,14 @@
-import { useNavigate } from "react-router-dom";
-import { useMemo } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { Fragment, useMemo } from "react";
 import jwt_decode from "jwt-decode";
 import { useAppDispatch, useAppSelector } from "../../../store";
 import { logoutUser } from "../../../redux/store/slice/auth";
 import ButtonFC from "../../components/utils/Button";
+import { Menu, Transition } from "@headlessui/react";
+
+function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(" ");
+}
 
 const Logo = () => {
     const navigate = useNavigate();
@@ -28,16 +33,82 @@ const Logo = () => {
     );
 };
 
+const menuDropDownItemClassName = (active: boolean) =>
+    classNames(
+        active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+        "block px-4 py-2 text-sm",
+    );
+
 const Menus = () => {
-    return <div className="flex bg-debug"><div id="menu-create" className="flex items-center">
-        s</div>
+    return (
+        <div className="flex ">
+            <div id="menu-create" className="flex items-center">
+                <Menu as="div" className="relative inline-block text-left">
+                    <div>
+                        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                            Create
+                        </Menu.Button>
+                    </div>
 
-        {/** HERE */}
-
-
-
-
-    </div>;
+                    <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                    >
+                        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <div className="py-1">
+                                <Menu.Item>
+                                    {({ active: itemActive }) => {
+                                        return (
+                                            <NavLink
+                                                to="/create/youtubeReference"
+                                                className={
+                                                    (/**{ isActive, isPending } */) =>
+                                                        menuDropDownItemClassName(itemActive)
+                                                }
+                                            >
+                                                Youtube Reference
+                                            </NavLink>
+                                        );
+                                    }}
+                                </Menu.Item>
+                                {/* <Menu.Item>
+                            {({ active }) => (
+                                <a
+                                    href="#"
+                                    className={classNames(
+                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                        'block px-4 py-2 text-sm'
+                                    )}
+                                >
+                                    Support
+                                </a>
+                            )}
+                        </Menu.Item>
+                        <Menu.Item>
+                            {({ active }) => (
+                                <a
+                                    href="#"
+                                    className={classNames(
+                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                        'block px-4 py-2 text-sm'
+                                    )}
+                                >
+                                    License
+                                </a>
+                            )}
+                        </Menu.Item> */}
+                            </div>
+                        </Menu.Items>
+                    </Transition>
+                </Menu>
+            </div>
+        </div>
+    );
 };
 
 const AuthenticatedNavItems = () => {
@@ -46,7 +117,7 @@ const AuthenticatedNavItems = () => {
     const dispatchLogout = () => dispatch(logoutUser());
 
     return (
-        <div id="authenticated-nav-items" className="flex space-x-3 border-2 border-black">
+        <div id="authenticated-nav-items" className="flex space-x-3 ">
             <Menus /> <ButtonFC text="logout" onClick={dispatchLogout} />{" "}
         </div>
     );
--- src/routes.tsx
+++ src/routes.tsx
@@ -8,7 +8,7 @@ import { useAppSelector } from "./store.tsx";
 
 const AuthWrapper = () => {
   const auth = useAppSelector((s) => s.auth);
-  if (auth.success) {
+  if (auth.token) {
     return <Outlet />;
   }
   return <div>Unauthenticated.</div>;
@@ -17,13 +17,21 @@ const AuthWrapper = () => {
 const Root = (
   <Route path="/" element={<DefaultLayout />}>
     <Route index element={<BodyHome />} />
+    <Route path="create">
+      <Route index element={<></>} />
+      <Route path="youtubeReference" element={<></>} />
+    </Route>
     <Route path="/diagnostics">
       <Route index element={<BodyDiagnostics />} />
       <Route path="ping" element={<BodyCounter />}></Route>
       <Route path="health" element={<AuthWrapper />}>
-        <Route path="auth" element={<div>healthcheck</div>}></Route>
+        <Route path="auth" element={<div>healthcheck-auth-ok</div>}></Route>
       </Route>
     </Route>
+
+    <Route path="*" element={<div className="flex justify-center">Oops! this page leads to nowhere.</div>} />
+
+
   </Route>
 );
 
